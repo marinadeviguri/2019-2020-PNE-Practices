e171a42e771cc0b1e54acc9ac72364eb17a03e20
marinadeviguri <marinadeviguri@gmail.com>
2020-04-22 22:13:47 +0200

    Session 5- EXERCISE 2

--- P0/Ex2.py
+++ P0/Ex2.py
@@ -1,5 +1,10 @@
 from Seq0 import *
 
-print("DNA file: ", filename)
+FOLDER = "../Session-04/"
+FILENAME = "U5.txt"
+DNA_FILE = FOLDER + FILENAME
+
+seq = seq_read_fasta(DNA_FILE)
+print("DNA file: ", FILENAME)
 print("The first 20 bases are: ")
-print(seq_read_fasta(U5.txt))
+print(seq[:20])
