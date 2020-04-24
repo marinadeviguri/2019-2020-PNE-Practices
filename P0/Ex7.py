d6714f84e2cfa999c07923b36d2a5803dd8cc39d
marinadeviguri <marinadeviguri@gmail.com>
2020-04-24 19:42:50 +0200

    Session 5- EXERCISE 7

---
+++ P0/Ex7.py
@@ -0,0 +1,15 @@
+from Seq0 import *
+
+PRACTICE = 7
+FOLDER = "../Session-04/"
+EXT = ".txt"
+GENES = ["U5", "ADA", "FRAT1", "FXN", "U5"]
+BASES = ['A', 'C', 'T', 'G']
+
+
+GENE = GENES[0]
+print(f"Gene {GENE}:")
+seq = seq_read_fasta(FOLDER + GENES[0] + EXT)[:20]
+comp = seq_complement(seq)
+print(f"Frag: {seq}")
+print(f"Comp: {comp}")
