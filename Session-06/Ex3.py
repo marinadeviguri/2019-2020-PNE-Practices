7627ad2f98402e4cd58aedf8a4512a4317b0aed4
marinadeviguri <marinadeviguri@gmail.com>
2020-05-07 11:25:13 +0200

    Exercise 3 - Session 6

---
+++ Session-06/Ex3.py
@@ -0,0 +1,37 @@
+class Seq:
+    def __init__(self, strbases):
+        bases = ['A', 'C', 'G', 'T']
+        self.strbases = strbases
+        print('New sequence created!')
+        for base in strbases:
+            if base not in bases:
+                print("ERROR!!")
+                self.strbases = "ERROR"
+                return
+    def __str__(self):
+        return self.strbases
+
+    def len(self):
+        return len(self.strbases)
+
+def print_seqs(seqs):
+    for seq in seqs:
+        print(f"Sequence {seqs.index(seq)}: (Length: {seq.len()}) {seq}")
+
+def generate_seqs(pattern, number):
+    sequences = []
+    for element in range(1, number + 1):
+        sequences.append(Seq(pattern * element))
+    return sequences
+
+
+# - Main program
+seq_list1 = generate_seqs("A", 3)
+seq_list2 = generate_seqs("AC", 5)
+
+print("List 1:")
+print_seqs(seq_list1)
+
+print()
+print("List 2:")
+print_seqs(seq_list2)
