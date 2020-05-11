fc5d95651bbddfff7d6b27fb81e20eedb3a1505f
marinadeviguri <marinadeviguri@gmail.com>
2020-05-11 18:33:30 +0200

    Session 10 - happy server

--- Session-10/happy-server.py
+++ Session-10/happy-server.py
@@ -11,8 +11,28 @@ ls.bind((IP, PORT))
 ls.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 ls.listen()
-
 print("Server is configured!")
 
+while True:
 
-ls.close()
+
+    try:
+        # --- Step 4: Wait for client to connect
+        (cs, client_ip_port) = ls.accept()
+    except KeyboardInterrupt:
+        print("Server is done!")
+        ls.close()
+        exit()
+    else:
+        # --- Step 5: Receiving information from the client
+        msg_raw = cs.recv(2000)
+        msg = msg_raw.decode()
+
+        print(f"Received message: ¨{msg}")
+
+    # --- Step 6: Send a response message to the client
+    response = "Hi! I am a happy server\n"
+    cs.send(response.encode())
+    cs.close()
+
+
